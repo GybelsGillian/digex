34c49ccf14641d4ee55ac97b5d0e03380d1721bb
Gillian Gybels <gillian.gybels@student.howest.be>
2026-07-14 11:04:32 +0200

    Add home page JavaScript and SCSS files; remove unused component styles

---
+++ src/js/pages/home.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Imprt scss files
+import '../../scss/pages/home.scss';
+
+// Import js files
+
+const initHome = function () {
+  console.log('Hello world!');
+};
+
+document.addEventListener('DOMContentLoaded', initHome);
